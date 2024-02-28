5f7db181c94844ada3455697f461670cb7229281
kunusaa <146696774+kunusaa@users.noreply.github.com>
2024-02-28 19:43:44 +0600

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "The translation of the word house?",
+        answer: "C. Үй",
+        options: [
+            "A. Ит",
+            "B. Гүл",
+            "C. Үй",
+            "D. Алмұрт"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The translation of the word nest?",
+        answer: "A. Ұя",
+        options: [
+            "A. Ұя",
+            "B. Орындық",
+            "C. Ұшақ",
+            "D. Үстел"
+        ]
+    },
+    {
+        numb: 3,
+        question: "The translation of the word tree?",
+        answer: "A. Ағаш",
+        options: [
+            "A. Ағаш",
+            "B. Алма",
+            "C. Төсек",
+            "D. Ет"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The translation of the word elephnat?",
+        answer: "D. Піл",
+        options: [
+            "A. Көктем",
+            "B. Саусақ",
+            "C. Кесіртке",
+            "D. Піл"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The translation of the word Spring?",
+        answer: "D. Көктем",
+        options: [
+            "A. Күз",
+            "B. Жаз",
+            "C. Қыс",
+            "D. Көктем"
+        ]
+    }
+];
